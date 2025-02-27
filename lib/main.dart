eaa84f64c7b068ee8ef2df65ffec798d21ff0a7e
Jooaomarcelo <joaomdsb@hotmail.com>
2025-02-27 12:59:26 -0300

    added user emails provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:controle_estoque_app/core/services/user_service.dart';
 import 'package:controle_estoque_app/pages/auth_page.dart';
 import 'package:controle_estoque_app/pages/products_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 import 'package:controle_estoque_app/pages/auth_or_app_page.dart';
@@ -21,49 +23,54 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromRGBO(60, 78, 105, 1),
-          primary: Color.fromRGBO(60, 78, 105, 1),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (ctx) => UserService()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Color.fromRGBO(60, 78, 105, 1),
+            primary: Color.fromRGBO(60, 78, 105, 1),
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromRGBO(60, 78, 105, 1),
+            foregroundColor: Colors.white,
+            titleTextStyle: TextStyle(
+              fontSize: 24,
+              fontFamily: 'Poppins',
+            ),
+            toolbarHeight: 80,
+          ),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(60, 78, 105, 1),
+            ),
+            labelMedium: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(60, 78, 105, 1),
+            ),
+            bodySmall: TextStyle(
+              fontSize: 16,
+              color: Colors.black54,
+            ),
+            bodyMedium: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          useMaterial3: true,
         ),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color.fromRGBO(60, 78, 105, 1),
-          foregroundColor: Colors.white,
-          titleTextStyle: TextStyle(
-            fontSize: 24,
-            fontFamily: 'Poppins',
-          ),
-          toolbarHeight: 80,
-        ),
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: Color.fromRGBO(60, 78, 105, 1),
-          ),
-          labelMedium: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Color.fromRGBO(60, 78, 105, 1),
-          ),
-          bodySmall: TextStyle(
-            fontSize: 16,
-            color: Colors.black54,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 16,
-          ),
-        ),
-        useMaterial3: true,
+        routes: {
+          '/': (ctx) => const AuthOrAppPage(),
+          '/auth': (ctx) => const AuthPage(),
+          '/products': (ctx) => const ProductsPage(),
+        },
       ),
-      routes: {
-        '/': (ctx) => const AuthOrAppPage(),
-        '/auth': (ctx) => const AuthPage(),
-        '/products': (ctx) => const ProductsPage(),
-      },
     );
   }
 }
