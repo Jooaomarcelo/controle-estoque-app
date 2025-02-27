1731f771010ac3def598a05bdac0cdd3fb3add99
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-02-27 15:29:29 -0300

    Colocando a barra de pesquisa

--- lib/pages/estoque1_page.dart
+++ lib/pages/estoque1_page.dart
@@ -1,3 +1,5 @@
+import 'package:controle_estoque_app/components/SearchProductBar.dart';
+import 'package:controle_estoque_app/pages/adicionar_estoque_page.dart';
 import 'package:flutter/material.dart';
 
 class EstoquePage1 extends StatelessWidget {
@@ -7,17 +9,47 @@ class EstoquePage1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         centerTitle: true,
         title: Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
-          Image.asset("assets/images/icone-produtos.png", width: 30),  
+          Image.asset(
+            'assets/images/estoque.png',
+            height: 30,
+            ),
+          const SizedBox(width: 15),
           const Text("Estoque"),
-          ]
+          ],
         ),
       ),
-      body: const Center(
-        child: Text("Nenhum estoque cadastrado."),
-      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+          child: Column(
+            children: [
+              SearchProductBar(),
+              const SizedBox(height: 15),
+              //StreamBuilder(),
+              const SizedBox(height: 100,)
+            ],
+          ),
+        ),
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AdicionarEstoquePage(),
+          ),
+        );
+      },
+      child: const Icon(Icons.add),
+    ),
+       
+      
     );
   }
 }
