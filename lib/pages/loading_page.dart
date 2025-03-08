699438efa282a448e355014138b7947ab2417cf9
Jooaomarcelo <joaomdsb@hotmail.com>
2025-03-08 17:17:35 -0300

    added animated loading screen

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
   const LoadingPage({super.key});
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+      lowerBound: 0.8,
+      upperBound: 1.0,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
-        child: Text('Loading Page'),
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _controller.value,
+              child: child,
+            );
+          },
+          child: SvgPicture.asset('assets/images/logo-dark.svg'),
+        ),
       ),
     );
   }
